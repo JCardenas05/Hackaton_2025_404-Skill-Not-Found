b4cb1de64c98482808f08c083dd6811d751218d5
Juan Daniel Gonzalez <112042600+JuanDanielGonzalez4@users.noreply.github.com>
2025-07-26 09:57:54 -0500

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/about" element={<div>About Page</div>} />
+    </Routes>
+  </BrowserRouter>
+);
